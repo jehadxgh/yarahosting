73d5cbb2ce00efd8860dc1ccfc778675b99700ce
jehadxgh <belal999777888@gmail.com>
2024-06-30 14:31:53 -0400

    Add files via upload

---
+++ src/aspCart.xUnitTest/Service.cs
@@ -0,0 +1,87 @@
+﻿using aspCart.Core.Domain.Catalog;
+using aspCart.Core.Domain.Messages;
+using aspCart.Core.Domain.Sale;
+using aspCart.Core.Domain.Statistics;
+using aspCart.Core.Domain.User;
+using aspCart.Infrastructure;
+using aspCart.Infrastructure.EFRepository;
+using aspCart.Infrastructure.Services.Catalog;
+using aspCart.Infrastructure.Services.Messages;
+using aspCart.Infrastructure.Services.Sale;
+using aspCart.Infrastructure.Services.Statistics;
+using aspCart.Infrastructure.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspCart.xUnitTest
+{
+    public class Service
+    {
+        public Service(ApplicationDbContext context)
+        {
+            // repository
+            BillingAddressRepository = new Repository<BillingAddress>(context);
+            CategoryRepository = new Repository<Category>(context);
+            ImageRepository = new Repository<Image>(context);
+            ManufacturerRepository = new Repository<Manufacturer>(context);
+            OrderRepository = new Repository<Order>(context);
+            OrderItemRepository = new Repository<OrderItem>(context);
+            ProductRepository = new Repository<Product>(context);
+            ProductCategoryMapping = new Repository<ProductCategoryMapping>(context);
+            ProductImageMapping = new Repository<ProductImageMapping>(context);
+            ProductManufacturerMapping = new Repository<ProductManufacturerMapping>(context);
+            ProductSpecificationMapping = new Repository<ProductSpecificationMapping>(context);
+            ReviewRepository = new Repository<Review>(context);
+            SpecificationRepository = new Repository<Specification>(context);
+            VisitorCountRepository = new Repository<VisitorCount>(context);
+            OrderCountRepository = new Repository<OrderCount>(context);
+            ContactUsMessageRepository = new Repository<ContactUsMessage>(context);
+
+            // service
+            BillingAddressService = new BillingAddressService(context, BillingAddressRepository);
+            CategoryService = new CategoryService(context, CategoryRepository, ProductCategoryMapping);
+            ManufacturerService = new ManufacturerService(context, ManufacturerRepository, ProductManufacturerMapping);
+            OrderService = new OrderService(context, OrderCountService, OrderRepository, OrderItemRepository);
+            ImageManagerService = new ImageManagerService(ImageRepository, ProductImageMapping);
+            ProductService = new ProductService(context, ProductRepository);
+            ReviewService = new ReviewService(context, ReviewRepository);
+            SpecificationService = new SpecificationService(context, SpecificationRepository, ProductSpecificationMapping);
+            VisitorCountService = new VisitorCountService(context, VisitorCountRepository);
+            OrderCountService = new OrderCountService(OrderCountRepository);
+            ContactUsService = new ContactUsService(ContactUsMessageRepository);
+        }
+
+        // repository
+        public Repository<BillingAddress> BillingAddressRepository { get; set; }
+        public Repository<Category> CategoryRepository { get; set; }
+        public Repository<Image> ImageRepository { get; set; }
+        public Repository<Manufacturer> ManufacturerRepository { get; set; }
+        public Repository<Order> OrderRepository { get; private set; }
+        public Repository<OrderItem> OrderItemRepository { get; private set; }
+        public Repository<Product> ProductRepository { get; set; }
+        public Repository<ProductCategoryMapping> ProductCategoryMapping { get; set; }
+        public Repository<ProductImageMapping> ProductImageMapping { get; set; }
+        public Repository<ProductManufacturerMapping> ProductManufacturerMapping { get; set; }
+        public Repository<ProductSpecificationMapping> ProductSpecificationMapping { get; set; }
+        public Repository<Review> ReviewRepository { get; set; }
+        public Repository<Specification> SpecificationRepository { get; set; }
+        public Repository<VisitorCount> VisitorCountRepository { get; set; }
+        public Repository<OrderCount> OrderCountRepository { get; set; }
+        public Repository<ContactUsMessage> ContactUsMessageRepository { get; set; }
+
+        // service
+        public BillingAddressService BillingAddressService { get; set; }
+        public CategoryService CategoryService { get; set; }
+        public ImageManagerService ImageManagerService { get; set; }
+        public ManufacturerService ManufacturerService { get; set; }
+        public OrderService OrderService { get; private set; }
+        public ProductService ProductService { get; set; }
+        public ReviewService ReviewService { get; set; }
+        public SpecificationService SpecificationService { get; set; }
+        public VisitorCountService VisitorCountService { get; set; }
+        public OrderCountService OrderCountService { get; set; }
+        public ContactUsService ContactUsService { get; set; }
+    }
+}
---
+++ src/aspCart.xUnitTest/ServiceTest/Catalog/ImageManagerService_Test.cs
@@ -0,0 +1,221 @@
+﻿using aspCart.Core.Domain.Catalog;
+using aspCart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace aspCart.xUnitTest.ServiceTest.Catalog
+{
+    public class ImageManagerService_Test
+    {
+        [Fact]
+        public void ImageManagerService_Test_GetAllImages()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_GetAllImages")
+                .Options;
+
+            var imageEntities = new List<Image>()
+            {
+                new Image { Id = Guid.NewGuid(), FileName = "Image 1" },
+                new Image { Id = Guid.NewGuid(), FileName = "Image 2" },
+                new Image { Id = Guid.NewGuid(), FileName = "Image 3" }
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                foreach (var image in imageEntities)
+                    context.Images.Add(image);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+                // assert
+                Assert.Equal(imageEntities.Count, service.ImageManagerService.GetAllImages().Count);
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_GetImageById()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_GetImageById")
+                .Options;
+
+            var imageEntity = new Image { Id = Guid.NewGuid(), FileName = "Image 1" };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Images.Add(imageEntity);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+                // assert
+                Assert.NotNull(service.ImageManagerService.GetImageById(imageEntity.Id));
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_SearchImages()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_SearchImages")
+                .Options;
+
+            var imageEntity = new Image { Id = Guid.NewGuid(), FileName = "Image 1" };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Images.Add(imageEntity);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+                // assert
+                Assert.NotNull(service.ImageManagerService.SearchImages("Image"));
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_InsertImages()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_InsertImage")
+                .Options;
+
+            var imageEntities = new List<Image> { new Image { Id = Guid.NewGuid(), FileName = "Image 1" } };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+
+                // act
+                service.ImageManagerService.InsertImages(imageEntities);
+
+                // assert
+                Assert.Equal(1, service.ImageManagerService.GetAllImages().Count);
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_DeleteImages()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_DeleteImages")
+                .Options;
+
+            var imageEntity = new Image { Id = Guid.NewGuid(), FileName = "Image 1" };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Images.Add(imageEntity);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+
+                // act
+                service.ImageManagerService.DeleteImages(new List<Guid> { imageEntity.Id });
+
+                // assert
+                Assert.Equal(0, service.ImageManagerService.GetAllImages().Count);
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_InsertProductImageMappings()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_InsertProductImageMappings")
+                .Options;
+
+            var productEntity = new Product() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100m };
+            var image1Entity = new Image() { Id = Guid.NewGuid(), FileName = "Image 1" };
+            var image2Entity = new Image() { Id = Guid.NewGuid(), FileName = "Image 2" };
+
+            var imageMappings = new List<ProductImageMapping>()
+            {
+                new ProductImageMapping() { Id = Guid.NewGuid(), ProductId = productEntity.Id, ImageId = image1Entity.Id },
+                new ProductImageMapping() { Id = Guid.NewGuid(), ProductId = productEntity.Id, ImageId = image2Entity.Id }
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Products.Add(productEntity);
+                context.Images.Add(image1Entity);
+                context.Images.Add(image2Entity);
+                context.SaveChanges();
+            }
+
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+
+                // act
+                service.ImageManagerService.InsertProductImageMappings(imageMappings);
+
+                // assert
+                Assert.Equal(imageMappings.Count, service.ProductService.GetProductById(productEntity.Id).Images.Count);
+            }
+        }
+
+        [Fact]
+        public void ImageManagerService_Test_DeleteAllProductImageMappings()
+        {
+            // arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "ImageManagerService_Test_DeleteAllProductImageMappings")
+                .Options;
+
+            var productEntity = new Product() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100m };
+            var image1Entity = new Image() { Id = Guid.NewGuid(), FileName = "Image 1" };
+            var image2Entity = new Image() { Id = Guid.NewGuid(), FileName = "Image 2" };
+
+            var imageMappings = new List<ProductImageMapping>()
+            {
+                new ProductImageMapping() { Id = Guid.NewGuid(), ProductId = productEntity.Id, ImageId = image1Entity.Id },
+                new ProductImageMapping() { Id = Guid.NewGuid(), ProductId = productEntity.Id, ImageId = image2Entity.Id }
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Products.Add(productEntity);
+                context.Images.Add(image1Entity);
+                context.Images.Add(image2Entity);
+                foreach (var mapping in imageMappings)
+                    context.ProductImageMappings.Add(mapping);
+                context.SaveChanges();
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new Service(context);
+
+                // act
+                service.ImageManagerService.DeleteAllProductImageMappings(productEntity.Id);
+
+                // assert
+                Assert.Equal(0, service.ProductService.GetProductById(productEntity.Id).Images.Count);
+            }
+        }
+    }
+}
